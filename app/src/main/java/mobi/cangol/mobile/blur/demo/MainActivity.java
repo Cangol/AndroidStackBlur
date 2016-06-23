8d4da1b2634654c7d325216782113e2b6167d7ac
Cangol <wxw404@gmail.com>
2016-06-24 00:05:18 +0800

    update demo

--- app/src/main/java/mobi/cangol/mobile/blur/demo/MainActivity.java
+++ app/src/main/java/mobi/cangol/mobile/blur/demo/MainActivity.java
@@ -1,8 +1,10 @@
 package mobi.cangol.mobile.blur.demo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import mobi.cangol.mobile.blur.StackBlurManager;
@@ -10,15 +12,35 @@ import mobi.cangol.mobile.blur.StackBlurManager;
 public class MainActivity extends Activity {
 
     private static final String TAG="Blur";
-    private TextView textView;
+    private ImageView imageView0;
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private TextView textView0;
+    private TextView textView1;
+    private TextView textView2;
+
     private StackBlurManager stackBlurManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView= (TextView) findViewById(R.id.textView);
-        stackBlurManager=new StackBlurManager(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        textView0= (TextView) findViewById(R.id.textView0);
+        textView1= (TextView) findViewById(R.id.textView1);
+        textView2= (TextView) findViewById(R.id.textView2);
 
-        stackBlurManager.processJava(10);
+        imageView0= (ImageView) findViewById(R.id.imageView0);
+        imageView1= (ImageView) findViewById(R.id.imageView1);
+        imageView2= (ImageView) findViewById(R.id.imageView2);
+        stackBlurManager=new StackBlurManager(BitmapFactory.decodeResource(getResources(),R.mipmap.test));
+
+        long curTime1=System.currentTimeMillis();
+        Bitmap bitmap1=stackBlurManager.processJava(70);
+        textView1.setText("Java:"+(System.currentTimeMillis()-curTime1)+"ms");
+        imageView1.setImageBitmap(bitmap1);
+
+        long curTime2=System.currentTimeMillis();
+        Bitmap bitmap2=stackBlurManager.processNative(70);
+        textView2.setText("Native:"+(System.currentTimeMillis()-curTime2)+"ms");
+        imageView2.setImageBitmap(bitmap2);
     }
 }
